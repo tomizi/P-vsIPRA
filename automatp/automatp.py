6eb96c2522531c6b75af0c7e78f43f913687338f
tomizi <106354750+tomizi@users.noreply.github.com>
2022-09-18 14:32:38 +0200

    Update automatp.py

--- automatp/automatp.py
+++ automatp/automatp.py
@@ -206,7 +206,7 @@ if rap_prom is not None or ipra is not None:
         
         ##
         
-        l, m, r = st.columns((1,1,1/2))
+        l, m, r = st.columns((1/2,1/2,1/2))
         with st.container():
             with l:
                 st.plotly_chart(px.scatter(nowy,x='Rabat IPRA WHA',y='Rabat Promocyjny').update_layout(
