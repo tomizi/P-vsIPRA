eefd6e4da65bcd573a12d329f17cf78eb39f5cc4
tomizi <106354750+tomizi@users.noreply.github.com>
2022-09-18 13:51:04 +0200

    Update automatp.py

--- automatp/automatp.py
+++ automatp/automatp.py
@@ -199,7 +199,7 @@ if rap_prom is not None or ipra is not None:
         
         ##
         l, m, r = st.columns(1,1,1)
-        
+        '''
         with l:
             st.plotly_chart(px.scatter(nowy,x='Rabat IPRA WHA',y='Rabat Promocyjny').update_layout(
             shapes=[
@@ -231,7 +231,7 @@ if rap_prom is not None or ipra is not None:
                 )
             ]))
             
-            
+        '''    
         st.plotly_chart(px.histogram(nowy['Rabat Promocyjny'],text_auto=True,marginal='box'))
         
         st.plotly_chart(px.histogram(nowy['Rabat IPRA WHA'],text_auto=True,marginal='box'))
