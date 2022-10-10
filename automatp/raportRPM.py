f308cfe7981c9215bfdc9d71be74afb145737ad8
tomizi <106354750+tomizi@users.noreply.github.com>
2022-10-10 11:46:33 +0200

    Update raportRPM.py

--- automatp/raportRPM.py
+++ automatp/raportRPM.py
@@ -39,6 +39,7 @@ if file is not None:
             Suma_końcowa[str(i)]=sum(piv[str(i)])
         piv.loc['Suma końcowa'] = Suma_końcowa
         st.dataframe(piv.style.format('{:.2f}'))
+        piv=piv.reset_index()
         st.download_button(label = 'Pobierz Raport RPM PKMH', data = piv.to_csv(index=False,encoding = 'utf-8'),file_name = 'Raport RPM RKMH.csv', mime = "text/csv")
 
     except Exception as e:
